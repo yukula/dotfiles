296f94952beb26dd92d0f2b968c582fc6daaacb2
Uladzislau Yukhno <uladzislau.yukhno@gmail.com>
2021-08-04 14:33:40 +0000

    Update colorscheme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,16 @@
 set fish_greeting
 
-#function fish_prompt
-	# requires JetBrains Mono
-	#printf " \u2248 "
-#end
-
 set -x EDITOR nvim
 
 set -x PATH $PATH ~/.local/bin
 
 
-set -x GDK_BACKEND wayland
-set -x MOZ_ENABLE_WAYLAND 1
+#set -x GDK_BACKEND wayland
+#set -x MOZ_ENABLE_WAYLAND 1
 
-set -x SDL_VIDEODRIVER wayland
+#set -x SDL_VIDEODRIVER wayland
 
-set -x QT_QPA_PLATFORM wayland-egl
-set -x QT_WAYLAND_FORCE_DPI physical
-set -x QT_QPA_PLATFORM wayland
-set -x QT_WAYLAND_DISABLE_WINDOWDECORATION 1
+#set -x QT_QPA_PLATFORM wayland-egl
+#set -x QT_WAYLAND_FORCE_DPI physical
+#set -x QT_QPA_PLATFORM wayland
+#set -x QT_WAYLAND_DISABLE_WINDOWDECORATION 1
